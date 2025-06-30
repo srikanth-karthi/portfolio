345bef6f7e2a0bf0155a2e77a385756351573f55
srikanth-karthi <srikanthkarthi2003@gmail.com>
2025-06-30 23:31:45 +0530

    Restore base path configuration for GitHub Pages in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  // base: '/portfolio/', // 👈 required for GitHub Pages
+  base: '/portfolio/', // 👈 required for GitHub Pages
   plugins: [react()],
 });
