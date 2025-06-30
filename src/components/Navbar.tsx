d5b708f463ea6a5b1fe21210ab58d15bfdef7a36
srikanth-karthi <srikanthkarthi2003@gmail.com>
2025-06-30 23:30:28 +0530

    Refactor package and component imports to use gsap directly; update gsap version in package.json

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,8 +1,9 @@
 import { useEffect } from "react";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { ScrollSmoother } from "gsap/ScrollSmoother";
 import HoverLinks from "./HoverLinks";
 import { gsap } from "gsap";
-import { ScrollSmoother } from "gsap-trial/ScrollSmoother";
+
 import "./styles/Navbar.css";
 
 gsap.registerPlugin(ScrollSmoother, ScrollTrigger);
